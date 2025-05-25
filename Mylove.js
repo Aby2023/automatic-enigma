0f976a35df85e4de6db6f1dc4fd19dc73d918fa9
Aby2023 <mikaelacc2024@gmail.com>
2025-05-24 21:41:20 -0500

    Create Mylove.js

---
+++ Mylove.js
@@ -0,0 +1,11 @@
+modificarTexto=function(){
+    let cmpTitulo1;
+    cmpTitulo1 = document.getElementById("titulo1");
+    cmpTitulo1.innerText = "MUAK PA' MI GUAPO"
+}
+
+modificarImagen=function(){
+    let cmpImagen1;
+   cmpImagen1=document.getElementById("imagen1");
+   cmpImagen1.src="love.jpg"
+}
